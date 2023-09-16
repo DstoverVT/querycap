3f7aa93e0962bd95140a7770ebd58daf01bed79e
Danny Stover <danielstover@vt.edu>
2023-09-17 00:01:35 +0100

    Working pi code.

--- Pi/pi_controller.py
+++ Pi/pi_controller.py
@@ -1,87 +1,128 @@
 import cv2
-import time
 import os
 import pyaudio
 import wave
+import requests
+import threading
+
 
 class QueryCapPi:
-    '''Controls RPi image and audio for QueryCap.'''
+    """Controls RPi image and audio for QueryCap."""
 
-    def __init__(self, audio_path, image_path):
+    user_input = None
+
+    def __init__(self, audio_path, image_path, server_url):
         self.cap_path = image_path
         self.query_path = audio_path
+        self.server = server_url
 
         # Video setup
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture(-1)
         self.use_video = True
 
-        if(not self.video.isOpened()):
+        if not self.video.isOpened():
+            print("Error opening webcam: not using video")
             self.use_video = False
 
         # Audio setup
         self.audio = pyaudio.PyAudio()
-        self.audio_chunk = 1024
-        # 16 kHz
-        self.audio_rate = 16000
-        self.audio_format = pyaudio.paInt16
 
-    
     def run(self):
-        '''Captures and image and then records the user's question'''
-        while(True):
+        """Captures and image and then records the user's question"""
+        print("Press Enter to start")
+        while True:
             key = input()
-            if(key == ''):
+            # Start image and audio capture on "enter" key press
+            if key is not None:
                 self.image_capture()
-                self.audio_capture(5)
+                # audio_capture waits for another "enter" press before done
+                self.audio_capture()
+                self.post_request()
 
+    def post_request(self):
+        """Sends image and audio files to server"""
+        files = {
+            "image": open(f"{self.cap_path}/cap.png", "rb"),
+            "sound": open(f"{self.query_path}/query.wav", "rb"),
+        }
+        response = requests.post(self.server, files=files)
+
+        SUCCESS = 200
+        if response.status_code == SUCCESS:
+            print("Data sent to server.")
+        else:
+            print(f"Error sending POST to {self.server}")
 
     def image_capture(self):
-        '''Capture frames from webcam.'''
-        if(self.use_video):
+        """Capture frames from webcam."""
+        if self.use_video:
             ret, frame = self.video.read()
             # Save 1 frame per second to path
-            if(ret):
+            if ret:
                 print(f"saving image")
                 cv2.imwrite(f"{self.cap_path}/cap.png", frame)
             else:
                 print(f"frame invalid")
 
+    def audio_capture(self):
+        """Captures audio from microphone."""
+        audio_chunk = 1024
+        # 16 kHz
+        audio_rate = 16000
+        audio_format = pyaudio.paInt16
 
-    def audio_capture(self, duration):
-        '''Captures audio from microphone.'''
-        self.audio_stream = self.audio.open(format=self.audio_format,
-                            channels=1,
-                            rate=self.audio_rate,
-                            input=True,
-                            frames_per_buffer=self.audio_chunk)
+        audio_stream = self.audio.open(
+            format=audio_format,
+            channels=1,
+            rate=audio_rate,
+            input=True,
+            frames_per_buffer=audio_chunk,
+        )
+        # Stores audio from audio stream
         recording = []
-        begin = time.time()
+
+        # Read console input in separate thread to determine when to stop recording
+        input_thread = threading.Thread(target=self.get_input)
+        input_thread.start()
 
         print("capturing audio...")
-        while((time.time() - begin) < duration):
+        while True:
             # Start recording audio
-            audio_frame = self.audio_stream.read(self.audio_chunk)
+            audio_frame = audio_stream.read(audio_chunk)
             recording.append(audio_frame)
+
+            # Stop recording audio on "enter" key press
+            if self.user_input is not None:
+                self.user_input = None
+                break
         print("Done")
+
+        audio_stream.stop_stream()
+        audio_stream.close()
 
         # Save it to a .wav file
         filename = "query.wav"
-        with wave.open(f'{self.query_path}/{filename}', 'wb') as wav:
+        with wave.open(f"{self.query_path}/{filename}", "wb") as wav:
             wav.setnchannels(1)
-            wav.setsampwidth(self.audio.get_sample_size(self.audio_format))
-            wav.setframerate(self.audio_rate)
-            wav.writeframes(b''.join(recording))
+            wav.setsampwidth(self.audio.get_sample_size(audio_format))
+            wav.setframerate(audio_rate)
+            wav.writeframes(b"".join(recording))
 
-        
-if __name__ == '__main__':
+    def get_input(self):
+        """Get keyboard input, runs in separate thread so non-blocking."""
+        self.user_input = input()
+
+
+if __name__ == "__main__":
     image_path = "images"
     audio_path = "audio"
+    URL = "http://172.29.103.161:5000/send_image"
 
     if not os.path.exists(image_path):
         os.makedirs(image_path)
     if not os.path.exists(audio_path):
         os.makedirs(audio_path)
 
-    # image_capture(path, FPS=1)
-    query_cap = QueryCapPi(audio_path, image_path)
+    query_cap = QueryCapPi(audio_path, image_path, URL)
     query_cap.run()
+    # query_cap.post_request()
